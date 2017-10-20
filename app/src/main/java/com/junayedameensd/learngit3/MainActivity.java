7f5aabcb195f2b0b271fbb93176f2a5f1070659f
Junayed Ameen <junayedameen@gmail.com>
2017-10-20 12:06:51 +0600

    Initial commit

---
+++ app/src/main/java/com/junayedameensd/learngit3/MainActivity.java
@@ -0,0 +1,21 @@
+package com.junayedameensd.learngit3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    String name;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setTitle("Commit 1");
+    }
+
+    private void nameMethod() {
+        name = "commit 1";
+    }
+}
